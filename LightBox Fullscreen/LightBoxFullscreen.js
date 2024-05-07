a427b758a8f0b52cf83129507474bd01b9d88a9e
JasonDeee <86407960+JasonDeee@users.noreply.github.com>
2024-05-07 08:53:01 +0700

    V1.0a Complete

--- LightBox Fullscreen/LightBoxFullscreen.js
+++ LightBox Fullscreen/LightBoxFullscreen.js
@@ -1,47 +1,62 @@
 // Vanced Images LightBox FullScreen v1.0a
-var isTheLightBoxReady = false;
-const Vx_LightBoxHolders = document.querySelectorAll(".Vx_LightBoxHolder");
+// 1.0a: Hỗ trợ cho Gallery
 
-for (
-  let Holder_index = 0;
-  Holder_index < Vx_LightBoxHolders.length;
-  Holder_index++
-) {
-  let Vx_LightBoxIms = Vx_LightBoxHolders[Holder_index].querySelectorAll(
-    ".ladi-gallery-view-item"
-  );
+const Vx_LightBoxHolders_Gallery = document.querySelectorAll(
+  ".Vx_LightBoxHolder .ladi-gallery"
+); // Chọn hết tất cả các phần tử là Gallery có áp dụng LightBox
 
-  let Vx_PseudoIms = document.createElement("div");
-  Vx_PseudoIms.style = "display: none";
-  Vx_LightBoxHolders[Holder_index].appendChild(Vx_PseudoIms);
+if (Vx_LightBoxHolders_Gallery != null) {
+  Vx_LightBoxHolders_Gallery.forEach((VxLighBoxEls_Gallery) => {
+    // Mỗi ảnh trong từng phần tử Gallery sẽ được lựa chọn
+    let Vx_LightBoxIms = VxLighBoxEls_Gallery.querySelectorAll(
+      ".ladi-gallery-view-item"
+    );
 
-  let Vx_FullscreenGarelly = null;
+    let isTheLightBoxReady = false; // Dùng để xác thực "đã bấm vào Gallery LẦN ĐẦU hay CHƯA"
 
-  function Vx_FullscreenShowFunc(Img_Index) {
-    if (isTheLightBoxReady == false) {
-      //
-      for (let index = 0; index < Vx_LightBoxIms.length; index++) {
+    // Bắt đầu tạo một DIV ảo để copy các ảnh vào trong đây TẠM THỜI
+    let Vx_PseudoIms = document.createElement("div");
+    Vx_PseudoIms.style = "display: none"; // Div ảo xuất hiện trong DOM nhưng không hiện thị
+    VxLighBoxEls_Gallery.appendChild(Vx_PseudoIms);
+    // Khởi tạo xog Div ảo
+
+    // Tạo trước một giá trị Null, sau này sẽ dùng làm Container cho LightBox (Object dạng Biến của JS ko phải DOM)
+    let Vx_FullscreenGarelly = null;
+
+    function Vx_FullscreenShowFunc(Img_Index) {
+      // Vì web có sử dụng Lazyload > nếu khởi tạo ngay từ đầu có thể phát sinh lỗi
+      // Lần đầu bấm vào Gallery mới khởi tạo LightBox
+
+      // Lần đầu Gallery: khởi tạo LightBox
+      if (isTheLightBoxReady == false) {
         //
-        let PseudoImages = document.createElement("img");
-        PseudoImages.style = "display: none";
-        let style =
-          Vx_LightBoxIms[index].currentStyle ||
-          window.getComputedStyle(Vx_LightBoxIms[index], false);
-        PseudoImages.src = style.backgroundImage.slice(4, -1).replace(/"/g, "");
-        Vx_PseudoIms.appendChild(PseudoImages);
+        for (let index = 0; index < Vx_LightBoxIms.length; index++) {
+          //
+          let PseudoImages = document.createElement("img");
+          PseudoImages.style = "display: none";
+          let style =
+            Vx_LightBoxIms[index].currentStyle ||
+            window.getComputedStyle(Vx_LightBoxIms[index], false);
+          PseudoImages.src = style.backgroundImage
+            .slice(4, -1)
+            .replace(/"/g, "");
+          Vx_PseudoIms.appendChild(PseudoImages);
+        }
+        Vx_FullscreenGarelly = new Viewer(Vx_PseudoIms);
+        Vx_FullscreenGarelly.view(Img_Index);
+        isTheLightBoxReady = true;
+      } else if (isTheLightBoxReady == true) {
+        // Kể từ lần thứ 2: Sử dụng LightBox đã khởi tạo và chạy với Index của ảnh bấm phải (Bắt đầu từ 0)
+        Vx_FullscreenGarelly.view(Img_Index);
       }
-      Vx_FullscreenGarelly = new Viewer(Vx_PseudoIms);
-      Vx_FullscreenGarelly.view(Img_Index);
-      isTheLightBoxReady = true;
-    } else if (isTheLightBoxReady == true) {
     }
-    Vx_FullscreenGarelly.view(Img_Index);
-  }
 
-  for (let Tindex = 0; Tindex < Vx_LightBoxIms.length; Tindex++) {
-    Vx_LightBoxIms[Tindex].addEventListener("click", () => {
-      Vx_FullscreenShowFunc(Tindex);
-      console.log(Tindex);
-    });
-  }
+    // Bước cuối: Thêm Trigger cho các ảnh (Click)
+    for (let Tindex = 0; Tindex < Vx_LightBoxIms.length; Tindex++) {
+      Vx_LightBoxIms[Tindex].addEventListener("click", () => {
+        Vx_FullscreenShowFunc(Tindex);
+        console.log(Tindex);
+      });
+    }
+  });
 }
